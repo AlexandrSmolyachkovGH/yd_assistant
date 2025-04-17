a706ed32e724849e7b2916fb894781ec8d7e938d
AlexandrSmolyachkovGH <alexandr.smolyachkov@gmail.com>
2025-04-18 01:16:32 +0300

    feat: Getting logins from Yandex Direct. Automated the creation of buttons and moved them out separately.

---
+++ bot/routers/report_buttons/client_report.py
@@ -0,0 +1,22 @@
+from aiogram.types import InlineKeyboardButton
+
+buttons_conf = [
+    ("Получить логины", "get_logins"),
+    ("Получить статистику", "get_base"),
+    ("Получить баланс", "get_balance"),
+    ("Справка", "daily_report"),
+    ("Назад", "back_to_report_menu"),
+]
+
+client_report_buttons = [[]]
+
+buttons = [
+    InlineKeyboardButton(text=f"{elem[0]}", callback_data=f"{elem[1]})") for elem in buttons_conf
+]
+
+for btn in buttons:
+    if len(client_report_buttons[-1]) < 2:
+        client_report_buttons[-1].append(btn)
+    else:
+        client_report_buttons.append([])
+        client_report_buttons[-1].append(btn)
